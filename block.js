d759a508c4af7ef41517b7485074012ad3b175a9
Greg Knight <lyngvi@gmail.com>
2014-05-04 17:09:35 -0400

    Made options page so we can change which domains we want to blacklist

--- block.js
+++ block.js
@@ -2,28 +2,47 @@
 // Google recommends we make it more efficient by setting URLs to just the list of URLs to block; however, this seems to allow
 // URLs that are not blocked to load data from URLs that are blocked, which is self-defeating. So screw it.
 var _g_UrlMatch = new RegExp('([^:]+):\/\/([^\/]+)');
-var _g_DomainMatches = [
-	'taboola.com'
-];
+var _g_Config = null;
+
+function requestHandler(details) {
+	if (_g_Config == null)
+		return;
+	var retu = { };
+	var matched = false;
+	var breakdown = _g_UrlMatch.exec(details.url);
+	var domain = RegExp.$2;
+	for (var k = 0; k < _g_Config.domainList.length; ++k) {
+		var ind = domain.indexOf(_g_Config.domainList[k]);
+		if (ind == 0 || domain.substring(ind - 1, ind) == '.') {
+			matched = true;
+		}
+	}
+	if (matched) {
+		console.log("URL " + details.url + " matched filter, blocked.");
+		retu.cancel = true;
+	}
+	return retu;
+}
+
+function reloadOptions() {
+	chrome.storage.sync.get({
+			version: 1,
+			domainList: [],
+		}, function(config) {
+			_g_Config = config;
+		}
+	);
+}
+
+chrome.storage.onChanged.addListener(function(changes) {
+	if ("domainList" in changes)
+		{ reloadOptions(); }
+});
 
 chrome.webRequest.onBeforeRequest.addListener(
-	function(details) {
-		var retu = { };
-		var matched = false;
-		var breakdown = _g_UrlMatch.exec(details.url);
-		var domain = RegExp.$2;
-		for (var k = 0; k < _g_DomainMatches.length; ++k) {
-			var ind = domain.indexOf(_g_DomainMatches[k]);
-			if (ind == 0 || domain.substring(ind - 1, ind) == '.') {
-				matched = true;
-			}
-		}
-		if (matched) {
-			console.log("URL " + details.url + " matched filter, blocked.");
-			retu.cancel = true;
-		}
-		return retu;
-	},
-	{ "urls": ["<all_urls>"] },
-	["blocking"]
+	requestHandler
+	, { "urls": ["<all_urls>"] }
+	, ["blocking"]
 );
+
+reloadOptions();
---
+++ options.js
@@ -0,0 +1,40 @@
+// <!--
+
+window.addEventListener("load", function(ev) {
+	var domainList = document.getElementById("domainList");
+	var status = document.getElementById("status");
+	document.getElementById("save").addEventListener("click", function(ev) {
+		var out = [];
+		var newList = domainList.value.split(',');
+		for (var k = 0; k < newList.length; ++k) {
+			var domain = newList[k].trim();
+			if (domain == "")
+				continue;
+			out.push(domain);
+		}
+		// Save it
+		chrome.storage.sync.set({
+				version: 1,
+				domainList: newList,
+			},
+			function() { // callback
+				status.textContent = 'Options saved.';
+				setTimeout(function() { status.textContent = ''; }, 750);
+			}
+		);
+	});
+	chrome.storage.sync.get({
+			version: 1,
+			domainList: [],
+		}, function(config) {
+			var s = '';
+			for (var k = 0; k < config.domainList.length; ++k)
+				{ s += ', ' + config.domainList[k]; }
+			domainList.value = s.substring(s.length < 2 ? 0 : 2, s.length);
+			status.textContent = 'Options loaded.';
+			setTimeout(function() { status.textContent = ''; }, 750);
+		}
+	);
+}, false);
+
+// -->
